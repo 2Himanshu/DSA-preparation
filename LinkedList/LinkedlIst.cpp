176edce2c28aa117f2ff5a41d6df7188d29bd5d2
2Himanshu <himanshu31072001@gmail.com>
2022-02-17 22:39:37 +0530

    adding LinkedList

---
+++ LinkedList/LinkedlIst.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+struct node *head=NULL;
+
+void insert(int data){
+    struct node *newNode = new node();
+    if(head==NULL){
+    newNode->data = data;
+    newNode->next = head;
+    head = newNode;
+    }
+    else{
+        newNode->data = data;
+        newNode->next = head;
+        head = newNode;
+    }
+}
+
+void print(){
+    if(head==NULL){
+        cout<<"List is empty"<<endl;
+    }
+    else{
+        struct node *tmp = head;
+        while(tmp!=NULL){
+            cout<<tmp->data<<" ";
+            tmp = tmp->next;
+        }
+    }
+}
+
+int main(){
+
+    int n;
+    cout<<"enter the number of element to be inserted:"<<endl;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+       insert(a) 
+    }
+
+    print();
+
+    return 0;
+}
